a6715eb935ffcfed3e7b98f86644cbdd781c7489
Gustavo Marmentini <gustavomarini9@gmail.com>
2017-02-02 14:06:33 -0200

    Update menu.lua

--- lua/menu.lua
+++ lua/menu.lua
@@ -1,5 +1,5 @@
 local Net = _G.LuaNetworking
-
+--[[
 function info(id)
 	local peer = managers.network._session:peer(id)
 	local peerName = peer:name()
@@ -18,13 +18,13 @@ function info(id)
 	managers.chat:_receive_message(1, "Character", peerChar, Color.red)
 	managers.chat:_receive_message(1, "Level and rank", "(" .. peer:rank() .. ") " .. peerLevel, Color.red)
 end
-
+]]
 if Net:IsMultiplayer() then
 	local peer = managers.network._session:peer(id)
 	local meuId = Net:LocalPeerID()
 	local eu = Net:GetNameFromPeerID(meuIp)
 	local menu_options = {}
-	menu_options[#menu_options+1] = { text = eu, data = meuId, callback = info }
+	--menu_options[#menu_options+1] = { text = eu, data = meuId, callback = info }
 	for _, peer in pairs(managers.network:session():peers()) do
 		menu_options[#menu_options+1] = { text = peer:name(), data = peer:id(), callback = info }
 	end
