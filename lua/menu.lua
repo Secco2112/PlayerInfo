6d08146b0cff90564b2a6892c347fc0954f85aa0
Gustavo <gustavomarini9@gmail.com>
2017-02-02 21:22:33 -0200

    Initial commit

--- lua/menu.lua
+++ lua/menu.lua
@@ -1,7 +1,8 @@
 local Net = _G.LuaNetworking
---[[
+
 function info(id)
 	local peer = managers.network._session:peer(id)
+	--[[
 	local peerName = peer:name()
 	local peerIP = peer:ip()
 	local peerID = peer:id()
@@ -11,20 +12,39 @@ function info(id)
 	local peerPing = peer:qos()
 	local peerLevel = peer:level()
 	local peerRank = peer:rank()
+	]]
 	
-	managers.chat:_receive_message(1, "Player Info", "Infos of " .. Net:GetNameFromPeerID(id), Color.green)
-	managers.chat:_receive_message(1, "Name", peerName, Color.red)
-	managers.chat:_receive_message(1, "ID", peerID, Color.red)
-	managers.chat:_receive_message(1, "Character", peerChar, Color.red)
-	managers.chat:_receive_message(1, "Level and rank", "(" .. peer:rank() .. ") " .. peerLevel, Color.red)
+	Dallas	menu_russian
+	Wolf	menu_german
+	Chains	menu_spanish
+	Houston	menu_american
+	John Wick	menu_jowi
+	Hoxton	menu_old_hoxton
+	Clover	menu_female_1
+	
+	personagens = {
+		["russian"] = "Dallas",
+		["american"] = "Houston",
+		["german"] = "Wolf",
+		["spanish"] = "Chains",
+		["jowi"] = "John Wick",
+		["female_1"] = "Clover",
+	}
+	
+	local character = peer:character()
+	
+	managers.chat:_receive_message(1, "Player Info", "Infos of " .. peer:name(), Color.green)
+	managers.chat:_receive_message(1, "Name", peer:name(), Color.red)
+	managers.chat:_receive_message(1, "ID", peer:id(), Color.red)
+	managers.chat:_receive_message(1, "Character", personagens[tostring(character)], Color.red)
 end
-]]
+
 if Net:IsMultiplayer() then
 	local peer = managers.network._session:peer(id)
-	local meuId = Net:LocalPeerID()
-	local eu = Net:GetNameFromPeerID(meuIp)
+	local myId = Net:LocalPeerID()
+	local peerLocal = Net:GetNameFromPeerID(myId)
 	local menu_options = {}
-	--menu_options[#menu_options+1] = { text = eu, data = meuId, callback = info }
+	menu_options[#menu_options+1] = { text = peerLocal, data = myId, callback = info }
 	for _, peer in pairs(managers.network:session():peers()) do
 		menu_options[#menu_options+1] = { text = peer:name(), data = peer:id(), callback = info }
 	end
